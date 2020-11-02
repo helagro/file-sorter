b37d88529d2d34a2235f257869155227a899315d
Henrik Lagrosen <henrikl@tuta.io>
2020-11-02 19:39:14 +0100

    varied

--- planning/envTemplate.py
+++ planning/envTemplate.py
@@ -3,6 +3,6 @@ from sortRule import SortRule
 TRACKED_FOLDERS = []
 
 SORT_RULES = [
-    SortRule("/home/h/Downloads/img", "ext", [".jpeg", ".jpg", ".png", ".psd"]), #Images
-    SortRule("/home/h/Downloads/compressed", "ext", [".zip", ".7zip", ".rar"]) #Compressed
+    SortRule("/img", "ext", [".jpeg", ".jpg", ".png", ".psd"]), #Images
+    SortRule("/compressed", "ext", [".zip", ".7zip", ".rar"]) #Compressed
 ]
